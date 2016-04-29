0353963c75e0d57b40855d13ac7e498a071b7995
edhilker <edhilker@att.net>
2016-04-29 11:07:11 -0400

    Added files via upload

---
+++ name.js
@@ -0,0 +1,17 @@
+﻿
+var app1 = angular.module('app1', []);
+
+//angular.module('myApp1', []).controller('namesCtrl', function ($scope) {
+//app1.controller('namesCtrl', function ($scope) {
+//    $scope.names = [
+//         { name: 'Jani', country: 'Norway' },
+//         { name: 'Hege', country: 'Sweden' },
+//         { name: 'Kai', country: 'Denmark' }
+//     ];
+//});
+
+
+app1.controller('Ctrl1', function ($scope) {
+    $scope.firstname = "Jack";
+    $scope.lastname = "We";
+});
---
+++ pc.js
@@ -0,0 +1,19 @@
+﻿
+var app2 = angular.module('app2', []);
+
+//    app2.controller('personCtrl', function($scope) {
+//     $scope.firstName = "John";
+//    $scope.lastName = "Doe";
+//    $scope.fullName = function() {
+//        return $scope.firstName + " " + $scope.lastName;
+//    };
+//});
+
+app2.controller('Ctrl2', function ($scope) {
+    $scope.firstName = "Steve";
+    $scope.lastName = "D";
+});
+
+angular.element(document).ready(function () {
+    angular.bootstrap(document.getElementById('app2'), ['app2']);
+});
